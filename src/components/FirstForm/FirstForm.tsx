26ed31405e679054dc756f1eef8218092e502ed4
ppmarkek <104010858+ppmarkek@users.noreply.github.com>
2023-12-01 17:10:14 +0200

    feat: fix firstForm

--- src/components/FirstForm/FirstForm.tsx
+++ src/components/FirstForm/FirstForm.tsx
@@ -1,7 +1,8 @@
-import './style.css';
-import { formSchema } from '../../yupSchema';
+import React, { useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useNavigate } from 'react-router-dom';
+import { formSchema } from '../../yupSchema';
+import * as Yup from 'yup';
 import {
   setName,
   setAge,
@@ -11,7 +12,6 @@ import {
   setTermsAndConditions,
   setImage,
 } from '../../redux/formReducer';
-import { useNavigate } from 'react-router-dom';
 
 interface FormValues {
   name: string;
@@ -27,147 +27,128 @@ interface FormValues {
 const FirstForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const formRef = useRef<HTMLFormElement>(null);
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
-  const handleSubmit = (values: FormValues) => {
-    dispatch(setName(values.name));
-    dispatch(setAge(values.age));
-    dispatch(setEmail(values.email));
-    dispatch(setPassword(values.password));
-    dispatch(setGender(values.gender));
-    dispatch(setTermsAndConditions(values.termsAndConditions));
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setErrors({});
 
-    if (values.image) {
-      const reader = new FileReader();
+    if (!formRef.current) return;
 
-      reader.onload = async (event) => {
-        if (event.target?.result) {
-          dispatch(setImage(event.target.result.toString()));
-          navigate('/');
-        }
-      };
-      reader.readAsDataURL(values.image);
-    } else {
-      navigate('/');
+    const formData = new FormData(formRef.current);
+    const formValues: FormValues = {
+      name: formData.get('name') as string,
+      age: formData.get('age') as string,
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+      confirmPassword: formData.get('confirmPassword') as string,
+      gender: formData.get('gender') as string,
+      termsAndConditions: formData.get('termsAndConditions') === 'on',
+      image: formData.get('image') as File | null,
+    };
+
+    try {
+      await formSchema.validate(formValues, { abortEarly: false });
+
+      dispatch(setName(formValues.name));
+      dispatch(setAge(formValues.age));
+      dispatch(setEmail(formValues.email));
+      dispatch(setPassword(formValues.password));
+      dispatch(setGender(formValues.gender));
+      dispatch(setTermsAndConditions(formValues.termsAndConditions));
+
+      const imageFile = formValues.image;
+      if (imageFile && imageFile.size > 0) {
+        const reader = new FileReader();
+        reader.onload = (event: ProgressEvent<FileReader>) => {
+          if (event.target?.result) {
+            dispatch(setImage(event.target.result as string));
+            navigate('/');
+          }
+        };
+        reader.readAsDataURL(imageFile);
+      } else {
+        navigate('/');
+      }
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const newErrors = err.inner.reduce((acc, error) => {
+          if (error.path) acc[error.path] = error.message;
+          return acc;
+        }, {} as { [key: string]: string });
+        setErrors(newErrors);
+      }
     }
   };
 
   return (
     <div className={'wrapper'}>
-      <Formik
-        initialValues={{
-          name: '',
-          age: '',
-          email: '',
-          password: '',
-          confirmPassword: '',
-          gender: '',
-          termsAndConditions: false,
-          image: null,
-        }}
-        validationSchema={formSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ setFieldValue }) => (
-          <Form className="form">
-            <div className={'inputBox'}>
-              <p>Name</p>
-              <Field name="name" type="text" />
-              <ErrorMessage
-                name="name"
-                component="p"
-                className="errorMessage"
-              />
-            </div>
+      <form ref={formRef} onSubmit={handleSubmit} className="form">
+        <div className={'inputBox'}>
+          <p>Name</p>
+          <input name="name" type="text" />
+          {errors.name && <p className="errorMessage">{errors.name}</p>}
+        </div>
 
-            <div className={'inputBox'}>
-              <p>Age</p>
-              <Field name="age" type="number" />
-              <ErrorMessage name="age" component="p" className="errorMessage" />
-            </div>
+        <div className={'inputBox'}>
+          <p>Age</p>
+          <input name="age" type="number" />
+          {errors.age && <p className="errorMessage">{errors.age}</p>}
+        </div>
 
-            <div className={'inputBox'}>
-              <p>Email</p>
-              <Field name="email" type="email" />
-              <ErrorMessage
-                name="email"
-                component="p"
-                className="errorMessage"
-              />
-            </div>
+        <div className={'inputBox'}>
+          <p>Email</p>
+          <input name="email" type="email" />
+          {errors.email && <p className="errorMessage">{errors.email}</p>}
+        </div>
 
-            <div className={'inputBox'}>
-              <p>Password</p>
-              <Field name="password" type="password" />
-              <ErrorMessage
-                name="password"
-                component="p"
-                className="errorMessage"
-              />
-            </div>
+        <div className={'inputBox'}>
+          <p>Password</p>
+          <input name="password" type="password" />
+          {errors.password && <p className="errorMessage">{errors.password}</p>}
+        </div>
 
-            <div className={'inputBox'}>
-              <p>Confirm password</p>
-              <Field name="confirmPassword" type="password" />
-              <ErrorMessage
-                name="confirmPassword"
-                component="p"
-                className="errorMessage"
-              />
-            </div>
+        <div className={'inputBox'}>
+          <p>Confirm Password</p>
+          <input name="confirmPassword" type="password" />
+          {errors.confirmPassword && (
+            <p className="errorMessage">{errors.confirmPassword}</p>
+          )}
+        </div>
 
-            <div className={'selectGender'}>
-              <label className={'selectGenderBox'}>
-                <p>Men</p>
-                <Field type="radio" name="gender" value="male" />
-              </label>
-              <label className={'selectGenderBox'}>
-                <p>Women</p>
-                <Field type="radio" name="gender" value="female" />
-              </label>
-            </div>
-            <ErrorMessage
-              name="gender"
-              component="p"
-              className="errorMessage"
-            />
+        <div className={'selectGender'}>
+          <label className={'selectGenderBox'}>
+            <p>Men</p>
+            <input type="radio" name="gender" value="male" />
+          </label>
+          <label className={'selectGenderBox'}>
+            <p>Women</p>
+            <input type="radio" name="gender" value="female" />
+          </label>
+        </div>
+        {errors.gender && <p className="errorMessage">{errors.gender}</p>}
 
-            <div className={'termsAndConditions'}>
-              <label>
-                <p>Terms and Conditions</p>
-                <Field type="checkbox" name="termsAndConditions" />
-              </label>
-              <ErrorMessage
-                name="termsAndConditions"
-                component="p"
-                className="errorMessage"
-              />
-            </div>
-
-            <div className={'uploadImage'}>
-              <p>Upload image</p>
-              <input
-                name="image"
-                type="file"
-                onChange={(event) => {
-                  const file = event.target.files
-                    ? event.target.files[0]
-                    : null;
-                  setFieldValue('image', file);
-                }}
-              />
-              <ErrorMessage
-                name="image"
-                component="p"
-                className="errorMessage"
-              />
-            </div>
-
-            <button id="Submit" type="submit">
-              Submit
-            </button>
-          </Form>
+        <div className={'termsAndConditions'}>
+          <label>
+            <p>Terms and Conditions</p>
+            <input type="checkbox" name="termsAndConditions" />
+          </label>
+        </div>
+        {errors.termsAndConditions && (
+          <p className="errorMessage">{errors.termsAndConditions}</p>
         )}
-      </Formik>
+
+        <div className={'uploadImage'}>
+          <p>Upload Image</p>
+          <input name="image" type="file" />
+        </div>
+        {errors.image && <p className="errorMessage">{errors.image}</p>}
+
+        <button type="submit" id="Submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,46 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './style.css';
 
 const Main = () => {
+  const formReducer = useSelector((state: object) => state.formReducer);
+
   return (
     <div className="wrapper">
       <button>
-        <Link to={'/firstForm'}>Formik</Link>
+        <Link to={'/firstForm'}>Form</Link>
       </button>
       <button>
         <Link to={'/secondForm'}>React Hook Form</Link>
       </button>
+      {formReducer.name !== '' && (
+        <div className="allData">
+          <div className="dataBox">
+            <p>Name:</p>
+            <p>{formReducer.name}</p>
+          </div>
+          <div className="dataBox">
+            <p>Age:</p>
+            <p>{formReducer.age}</p>
+          </div>
+          <div className="dataBox">
+            <p>Email:</p>
+            <p>{formReducer.email}</p>
+          </div>
+          <div className="dataBox">
+            <p>Password:</p>
+            <p>{formReducer.password}</p>
+          </div>
+          <div className="dataBox">
+            <p>Gender:</p>
+            <p>{formReducer.gender}</p>
+          </div>
+          <div className="dataBox">
+            <p>Image:</p>
+            <img src={formReducer.image} alt="img" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
